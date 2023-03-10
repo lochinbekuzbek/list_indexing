7a912c81d04af12d363f7c3916bb9c64c7933c8b
lochinbekuzbek <rustamovlochin77@gmail.com>
2023-03-10 23:09:15 +0500

    list08

--- bin/list_index08.dart
+++ bin/list_index08.dart
@@ -9,8 +9,8 @@
 List func(List list1) {
   int a = list1.length;
   int b = 0;
-  String s = 'false';
-  String c = 'true';
+  String s = 'False';
+  String c = 'True';
   while (b < a) {
     if (list1[b] == 0) {
       list1[b] = s;
