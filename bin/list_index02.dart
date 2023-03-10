7e8cd4eac6dc7bbbe7116a603dbb12c8a5657d75
lochinbekuzbek <rustamovlochin77@gmail.com>
2023-03-10 15:40:41 +0500

    list02

--- bin/list_index02.dart
+++ bin/list_index02.dart
@@ -6,4 +6,11 @@
     Returns:
         list: return answer
 */
-void main() {}
+List func(list1) {
+  List a = [list1[0]];
+  return a;
+}
+
+void main() {
+  print(func([66, 3, 2, 1, 4, 4, 4]));
+}
