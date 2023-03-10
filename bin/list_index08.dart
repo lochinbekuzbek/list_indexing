5615ad081dc14d7e162fb7a24da0e3fc65c3c012
lochinbekuzbek <rustamovlochin77@gmail.com>
2023-03-10 17:14:41 +0500

    list8

--- bin/list_index08.dart
+++ bin/list_index08.dart
@@ -6,4 +6,19 @@
     Returns:
         list: return answer
 */
-void main() {}
+List func(List list1) {
+  int a = list1.length;
+  int b = 0;
+  String s = 'folse';
+  while (b < a) {
+    if (list1[b] == 0) {
+      list1[b] = s;
+    }
+    b++;
+  }
+  return list1;
+}
+
+void main() {
+  print(func([1, 0, 0, 0, 0]));
+}
