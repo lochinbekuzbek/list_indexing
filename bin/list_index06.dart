282e42a4f82f2611cb58061518775794a3425a24
lochinbekuzbek <rustamovlochin77@gmail.com>
2023-03-10 17:09:42 +0500

    list06

--- bin/list_index06.dart
+++ bin/list_index06.dart
@@ -6,4 +6,19 @@
     Returns:
         list: return answer
 */
-void main() {}
+List func(List list1) {
+  int a = list1.length;
+  int b = 0;
+  String s = 'true';
+  while (b < a) {
+    if (list1[b] == 0) {
+      list1[b] = s;
+    }
+    b++;
+  }
+  return list1;
+}
+
+void main() {
+  print(func([0, 0, 1, 1, 0]));
+}
